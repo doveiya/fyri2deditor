c0ad7677cca48624d5e8de2924cf0e2d98461b6c
dovieya@gmail.com <dovieya@gmail.com@7f793118-335c-354c-b040-1ba5961bb035>
2012-12-05 23:29:47 +0000

    Added RoundLine Demo

---
+++ trunk/Xna2dEditor/Models/IModelManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fyri2dEditor
+{
+    public interface IModelManager
+    {
+        void Initialize();
+        FyriModel LoadModel(string fileName);
+        FyriModel GetModel(string modelName);
+        List<FyriModel> RefreshList(List<FyriModel> list);
+    }
+}
